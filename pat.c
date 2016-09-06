0614b43235aad70afebe068597c7ae88c2af82c2
Tom Newman <tnewman1@ltu.edu>
2016-09-05 22:17:26 -0400

    Added get_pat_audio_stream to open the audio stream, format context and codec context.

--- pat.c
+++ pat.c
@@ -11,8 +11,9 @@ typedef struct PATAudioStream {
 
 SDL_AudioDeviceID output_device;
 SDL_AudioSpec actual_output_spec;
+int init_called = 0;
 
-int get_audio_format_context(const char* audio_file_path);
+int get_pat_audio_stream(const char* audio_file_path, PATAudioStream** pat_audio_stream);
 
 void free_pat_audio_stream(PATAudioStream** pat_audio_stream);
 
@@ -41,10 +42,80 @@ int pat_init() {
     
     av_register_all();
     
+    init_called = 1;
+    
     return PAT_SUCCESS;
 }
 
-int get_audio_stream(const char* audio_file_path) {
+int get_pat_audio_stream(const char* audio_file_path, PATAudioStream** pat_audio_stream) {
+    free_pat_audio_stream(pat_audio_stream);    
+    
+    if(init_called == 0) {
+        return PAT_NOT_INITIALIZED_ERROR;
+    }
+    
+    *pat_audio_stream = malloc(sizeof(PATAudioStream));
+    
+    if(*pat_audio_stream == NULL) {
+        return PAT_OPEN_FILE_ERROR;
+    }
+    
+    (*pat_audio_stream)->audio_stream = NULL;
+    (*pat_audio_stream)->format_context = NULL;
+    (*pat_audio_stream)->codec_context = NULL;
+    
+    // AVFormatContext
+    AVFormatContext* format_context = NULL;
+    
+    if(avformat_open_input(&format_context, audio_file_path, NULL, NULL) != 0) {
+        free_pat_audio_stream(pat_audio_stream);
+        return PAT_OPEN_FILE_ERROR;
+    }
+    
+    (*pat_audio_stream)->format_context = format_context;
+    
+    if(avformat_find_stream_info(format_context, NULL) != 0) {
+        free_pat_audio_stream(pat_audio_stream);
+        return PAT_OPEN_FILE_ERROR;
+    }
+    
+    // AVStream
+    AVStream* audio_stream = NULL;
+    
+    for(int i = 0; i < format_context->nb_streams; i++) {
+        if(format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            audio_stream = format_context->streams[i];
+        }
+    }
+    
+    if(audio_stream == NULL) {
+        free_pat_audio_stream(pat_audio_stream);
+        return PAT_NO_AUDIO_STREAM_ERROR;
+    }
+    
+    (*pat_audio_stream)->audio_stream = audio_stream;
+    
+    // AVCodec
+    AVCodecParameters* codec_parameters = audio_stream->codecpar;
+    AVCodec* codec = avcodec_find_decoder(codec_parameters->codec_id);
+    
+    if(codec == NULL) {
+        free_pat_audio_stream(pat_audio_stream);
+        return PAT_CODEC_ERROR;
+    }
+    
+    AVCodecContext* codec_context = avcodec_alloc_context3(codec);
+    
+    if(codec_context == NULL) {
+        free_pat_audio_stream(pat_audio_stream);
+        return PAT_CODEC_ERROR;
+    }
+    
+    if(avcodec_parameters_to_context(codec_context, codec_parameters) < 0) {
+        free_pat_audio_stream(pat_audio_stream);
+        return PAT_CODEC_ERROR;
+    }
+    
     return PAT_SUCCESS;
 }
 
@@ -64,6 +135,10 @@ void free_pat_audio_stream(PATAudioStream** pat_audio_stream) {
     if(format_context != NULL) {
         avformat_close_input(&format_context);
     }
+    
+    free(*pat_audio_stream);
+    
+    *pat_audio_stream = NULL;
 }
 
 int pat_quit() {
@@ -75,11 +150,5 @@ int pat_quit() {
 }
 
 int main() {
-    int retval;
-    
-    retval = pat_init();
-    
-    printf("%d", retval);
-    
-    pat_quit();
+
 }
