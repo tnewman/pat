f1582a33b639c37d807a3d4da25b94eb68420901
Tom Newman <tnewman1@ltu.edu>
2016-10-08 12:42:58 -0400

    PyPi Release

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 pat_extension = Extension('pat', sources=['patmodule.c', 'pat.c'], 
     libraries=['avcodec', 'avformat', 'swresample', 'avutil', 'SDL2'])
 
-setup(name='pat', ext_modules=[pat_extension])
+setup(name='pythonaudiotechnician', 
+      description='PAT Audio Technician (PAT)',
+      version='0.0.6',
+      license='LGPLv3+',
+      author='Thomas Newman',
+      author_email='tnewman1@ltu.edu',
+      url='https://github.com/tnewman/pat',
+      download_url='https://github.com/tnewman/pat/releases',
+      ext_modules=[pat_extension],
+      platforms=['Linux', 'Windows'],
+      classifiers=[
+          'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: Microsoft :: Windows',
+      ])
