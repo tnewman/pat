78c4167a03a1505fd5ed042a713d0c1fde3b8eb3
Thomas Newman <tnewman@users.noreply.github.com>
2018-10-28 14:28:55 -0400

    Include Guard for Library

--- pat/include/pat/pat.h
+++ pat/include/pat/pat.h
@@ -1,3 +1,6 @@
+#ifndef PAT_H
+#define PAT_H
+
 typedef struct PAT PAT;
 
 PAT* pat_open();
@@ -5,3 +8,5 @@ PAT* pat_open();
 void pat_play(PAT* pat, char* pat_audio_path);
 
 void pat_close(PAT* pat);
+
+#endif
--- test/src/pat_test.c
+++ test/src/pat_test.c
@@ -1,7 +1,13 @@
 #include "pat/pat.h"
+#include <stdlib.h>
 
 int main(void) {
     PAT* pat = pat_open();
+
+    if(pat == NULL) {
+
+    }
+
     pat_play(pat, "/home/tnewman/Downloads/tilltheend.mpga");
     pat_close(pat);
     return 0;
