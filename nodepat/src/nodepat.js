387f23fc86fe6b5713fc4ca2f71849515e521ead
Thomas Newman <tnewman@users.noreply.github.com>
2018-12-08 11:52:43 -0500

    Run libpat on Node worker threads

--- nodepat/src/nodepat.js
+++ nodepat/src/nodepat.js
@@ -6,12 +6,12 @@ const ref = require('ref');
 const PATPtrType = ref.refType('void');
 const PATPtrPtrType = ref.refType(PATPtrType);
 
-const libpat = ffi.Library(__dirname + '/../libpat/build/bin/libpat', {
+const libpat = ffi.Library(__dirname + '/libpat/build/bin/libpat', {
     'pat_open': ['int', [PATPtrPtrType]],
-    'pat_play_async': ['int', [PATPtrType, 'string', 'pointer', 'Object']],
-    'pat_skip_async': ['int', [PATPtrType, 'pointer', 'Object']],
-    'pat_pause_async': ['int', [PATPtrType, 'pointer', 'Object']],
-    'pat_resume_async': ['int', [PATPtrType, 'pointer', 'Object']],
+    'pat_play': ['int', [PATPtrType, 'string']],
+    'pat_skip': ['int', [PATPtrType]],
+    'pat_pause': ['int', [PATPtrType]],
+    'pat_resume': ['int', [PATPtrType]],
     'pat_close': ['void', [PATPtrType]],
     'pat_error_to_string': ['string', ['int']]
 });
@@ -29,54 +29,52 @@ const PAT_UNKNOWN_ERROR = 9;
 
 let patPtr = open();
 
-let libpatCallback = ffi.Callback('void', ['int', 'Object'], (patError, promise) => {
-    if(patError === PAT_SUCCESS) {
-        promise.resolve();
-    } else {
-        promise.reject(new Error(libpat.pat_error_to_string(patError)));
-    }
-});
-
 function open() {
     const patPtrPtr = ref.alloc(PATPtrPtrType);
 
-    let error = libpat.pat_open(patPtrPtr);
-    checkStatus(error);
+    let status = libpat.pat_open(patPtrPtr);
+    checkPatStatus(status);
 
     return patPtrPtr.deref();
 }
 
 function play(audioPath) {
     return new Promise((resolve, reject) => {
-        let status = libpat.pat_play_async(patPtr, audioPath, libpatCallback, {resolve, reject});
-
-        if(status !== PAT_SUCCESS) {
-            reject(new Error(libpat.pat_error_to_string(status)))
-        }
+        libpat.pat_play.async(patPtr, audioPath, (err, res) => audioOperationCallback(err, res, resolve, reject));
     });
 }
 
 function skip() {
     return new Promise((resolve, reject) => {
-        libpat.pat_skip_async(patPtr, libpatCallback, {resolve, reject});
+        libpat.pat_skip.async(patPtr, (err, res) => audioOperationCallback(err, res, resolve, reject));
     });
 }
 
 function pause() {
     return new Promise((resolve, reject) => {
-        libpat.pat_pause_async(patPtr, libpatCallback, {resolve, reject});
+        libpat.pat_pause.async(patPtr, (err, res) => audioOperationCallback(err, res, resolve, reject));
     });
 }
 
 function resume() {
     return new Promise((resolve, reject) => {
-        libpat.pat_resume_async(patPtr, libpatCallback, {resolve, reject});
+        libpat.pat_resume.async(patPtr, (err, res) => audioOperationCallback(err, res, resolve, reject));
     });
 }
 
-function checkStatus(error) {
+function checkPatStatus(error) {
     if (error !== PAT_SUCCESS) {
         throw new Error(libpat.pat_error_to_string(error));
+    }
+}
+
+function audioOperationCallback(err, res, resolve, reject) {
+    if (err) {
+        reject(err);
+    } else if (res !== PAT_SUCCESS) {
+        reject(new Error(libpat.pat_error_to_string(status)));
+    } else {
+        resolve();
     }
 }
 
--- nodepat/tools/prepack.js
+++ nodepat/tools/prepack.js
@@ -8,13 +8,15 @@ if(fs.existsSync(config.LIBPAT_DIR)) {
 
     fs.copySync(config.LIBPAT_DIR, config.LIBPAT_NODEPAT_DIR);
 
-    fs.removeSync(config.LIBPAT_BUILD_DIR);
+    fs.removeSync(config.LIBPAT_NODEPAT_BUILD_DIR);
 
     removeIfExists(config.LIBPAT_NODEPAT_CMAKE_DEBUG_DIR);
     removeIfExists(config.LIBPAT_NODEPAT_CMAKE_RELEASE_DIR);
     removeIfExists(config.LIBPAT_NODEPAT_IDEA_DIR);
+    removeIfExists(config.LIBPAT_NODEPAT_PAT_PLAY_DIR);
 
-    fs.ensureDirSync(config.LIBPAT_BUILD_DIR);
+    fs.ensureDirSync(config.LIBPAT_NODEPAT_BUILD_DIR);
+    fs.ensureFileSync(config.LIBPAT_NODEPAT_GITKEEP_FILE);
 }
 
 function removeIfExists(directory) {
