f4b8456dbb0abe0c0696101b68f1962f625b6ab5
Thomas Newman <tnewman@users.noreply.github.com>
2018-12-09 18:24:09 -0500

    Adjust timeout

--- nodepat/test/nodejs.js
+++ nodepat/test/nodejs.js
@@ -65,7 +65,7 @@ describe('pat', function() {
         let playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
             let elapsedTime = Date.now() - startTime;
 
-            if(elapsedTime < 2500) {
+            if(elapsedTime < 2100) {
                 assert.fail("pause did not occur");
             }
         });
@@ -85,7 +85,7 @@ describe('pat', function() {
         let playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
             let elapsedTime = Date.now() - startTime;
 
-            if(elapsedTime < 2500) {
+            if(elapsedTime < 2100) {
                 assert.fail("pause did not occur");
             }
         });
