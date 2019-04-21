bed3681cbb73c403caf12723f72869345ab1231a
Thomas Newman <tnewman@users.noreply.github.com>
2019-04-20 23:20:58 -0400

    Add ESLint

--- nodepat/test/nodejs.spec.ts
+++ nodepat/test/nodejs.spec.ts
@@ -1,137 +1,134 @@
 import assert from 'assert';
-import * as nodepat from '../src/nodepat'
+import path from 'path';
+import * as nodepat from '../src/nodepat';
 
-const VALID_AUDIO_PATH = __dirname + '/../src/libpat/test/test.mp3';
+const VALID_AUDIO_PATH = path.join(__dirname, '../src/libpat/test/test.mp3');
 const MISSING_AUDIO_PATH = 'missing';
-const INVALID_AUDIO_PATH = __dirname + '/../src/libpat/test/invalid.mp3';
+const INVALID_AUDIO_PATH = path.join(__dirname, '../src/libpat/test/invalid.mp3');
 
-describe('pat', function() {
-    const TIMEOUT = 5000;
+describe('pat', () => {
+  const TIMEOUT = 5000;
 
-    it('should play an audio file', function() {
-        return nodepat.play(VALID_AUDIO_PATH);
-    }).timeout(TIMEOUT);
+  it('should play an audio file', () => nodepat.play(VALID_AUDIO_PATH)).timeout(TIMEOUT);
 
-    it('should play an audio file synchronously', function() {
-        nodepat.playSync(VALID_AUDIO_PATH);
-    }).timeout(TIMEOUT);
+  it('should play an audio file synchronously', () => {
+    nodepat.playSync(VALID_AUDIO_PATH);
+  }).timeout(TIMEOUT);
 
-    it('should reject a missing audio file', function() {
-        return nodepat.play(MISSING_AUDIO_PATH).then(() => {
-            assert.fail("missing audio should not play");
-        }).catch((error) => {
-            assert.deepStrictEqual(error.message,
-                "Could not open the audio file. The audio file is inaccessible or does not exist.");
-        });
-    }).timeout(TIMEOUT);
+  it('should reject a missing audio file', () => nodepat.play(MISSING_AUDIO_PATH).then(() => {
+    assert.fail('missing audio should not play');
+  }).catch((error) => {
+    assert.deepStrictEqual(error.message,
+      'Could not open the audio file. The audio file is inaccessible or does not exist.');
+  })).timeout(TIMEOUT);
 
-    it('should reject a missing audio file synchronously', function() {
-        try {
-            nodepat.playSync(MISSING_AUDIO_PATH);
-        } catch(error) {
-            assert.deepStrictEqual(error.message,
-                "Could not open the audio file. The audio file is inaccessible or does not exist.");
-            return;
-        }
+  it('should reject a missing audio file synchronously', () => {
+    try {
+      nodepat.playSync(MISSING_AUDIO_PATH);
+    } catch (error) {
+      assert.deepStrictEqual(error.message,
+        'Could not open the audio file. The audio file is inaccessible or does not exist.');
+      return;
+    }
 
-        assert.fail("missing audio should not play");
-    }).timeout(TIMEOUT);
+    assert.fail('missing audio should not play');
+  }).timeout(TIMEOUT);
 
-    it('should reject an invalid audio file', function() {
-        return nodepat.play(INVALID_AUDIO_PATH).then(() => {
-            assert.fail("invalid audio should not play");
-        }).catch((error) => {
-            assert.deepStrictEqual(error.message,
-                "Could not demux the audio file. The audio file format is unsupported or corrupted, or " +
-                "the audio file does not contain an audio stream.");
-        });
-    }).timeout(TIMEOUT);
+  it('should reject an invalid audio file', () => nodepat.play(INVALID_AUDIO_PATH).then(() => {
+    assert.fail('invalid audio should not play');
+  }).catch((error) => {
+    assert.deepStrictEqual(error.message,
+      'Could not demux the audio file. The audio file format is unsupported or corrupted, or '
+        + 'the audio file does not contain an audio stream.');
+  })).timeout(TIMEOUT);
 
-    it('should reject an invalid audio file synchronously', function() {
-       try {
-           nodepat.playSync(INVALID_AUDIO_PATH);
-       }  catch(error) {
-           assert.deepStrictEqual(error.message, "Could not demux the audio file. The audio file format is " +
-               "unsupported or corrupted, or the audio file does not contain an audio stream.");
-           return;
-       }
+  it('should reject an invalid audio file synchronously', () => {
+    try {
+      nodepat.playSync(INVALID_AUDIO_PATH);
+    } catch (error) {
+      assert.deepStrictEqual(error.message, 'Could not demux the audio file. The audio file format is '
+        + 'unsupported or corrupted, or the audio file does not contain an audio stream.');
+      return;
+    }
 
-       assert.fail("invalid audio should not play");
-    }).timeout(TIMEOUT);
+    assert.fail('invalid audio should not play');
+  }).timeout(TIMEOUT);
 
-    it('should pause and resume an audio file', function() {
-        let startTime = Date.now();
+  it('should pause and resume an audio file', () => {
+    const startTime = Date.now();
 
-        let playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
-            let elapsedTime = Date.now() - startTime;
+    const playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
+      const elapsedTime = Date.now() - startTime;
 
-            if(elapsedTime < 1500) {
-                assert.fail("pause did not occur");
-            }
-        });
+      if (elapsedTime < 1500) {
+        assert.fail('pause did not occur');
+      }
+    });
 
-        nodepat.pause().then(() => {
-            setTimeout(() => {
-                nodepat.resume();
-            }, 1000);
-        });
+    nodepat.pause().then(() => {
+      setTimeout(() => {
+        nodepat.resume();
+      }, 1000);
+    });
 
-        return playPromise;
-    }).timeout(TIMEOUT);
+    return playPromise;
+  }).timeout(TIMEOUT);
 
-    it('should pause and resume an audio file synchronously', async function() {
-        let startTime = Date.now();
+  it('should pause and resume an audio file synchronously', async () => {
+    const startTime = Date.now();
 
-        let playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
-            let elapsedTime = Date.now() - startTime;
+    const playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
+      const elapsedTime = Date.now() - startTime;
 
-            if(elapsedTime < 1500) {
-                assert.fail("pause did not occur");
-            }
-        });
+      if (elapsedTime < 1500) {
+        assert.fail('pause did not occur');
+      }
+    });
 
-        // Setting a timeout is required to ensure pause is not called before the Node worker thread for play runs
-        setTimeout(() => {
-            nodepat.pauseSync();
-            setTimeout(() => nodepat.resume(), 1000);
-        }, 100);
+    // Setting a timeout is required to ensure pause is not called
+    // before the Node worker thread for play runs
+    setTimeout(() => {
+      nodepat.pauseSync();
+      setTimeout(() => nodepat.resume(), 1000);
+    }, 100);
 
-        return playPromise;
-    }).timeout(TIMEOUT);
+    return playPromise;
+  }).timeout(TIMEOUT);
 
-    it('should skip an audio file', function() {
-        let startTime = Date.now();
+  it('should skip an audio file', () => {
+    const startTime = Date.now();
 
-        let playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
-            let elapsedTime = Date.now() - startTime;
+    const playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
+      const elapsedTime = Date.now() - startTime;
 
-            if(elapsedTime > 500) {
-                assert.fail("skip did not occur");
-            }
-        });
+      if (elapsedTime > 500) {
+        assert.fail('skip did not occur');
+      }
+    });
 
-        nodepat.skip();
+    nodepat.skip();
 
-        return playPromise;
-    }).timeout(TIMEOUT);
+    return playPromise;
+  }).timeout(TIMEOUT);
 
-    it('should skip an audio file synchronously', async function() {
-        let startTime = Date.now();
+  it('should skip an audio file synchronously', async () => {
+    const startTime = Date.now();
 
-        let playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
-            let elapsedTime = Date.now() - startTime;
+    const playPromise = nodepat.play(VALID_AUDIO_PATH).then(() => {
+      const elapsedTime = Date.now() - startTime;
 
-            if(elapsedTime > 500) {
-                assert.fail("skip did not occur");
-            }
-        });
+      if (elapsedTime > 500) {
+        assert.fail('skip did not occur');
+      }
+    });
 
-        // Setting a timeout is required to ensure pause is not called before the Node worker thread for play runs
-        setTimeout(() => {
-            nodepat.pauseSync();
-            nodepat.skipSync();
-        }, 100);
+    // Setting a timeout is required to ensure pause is not
+    // called before the Node worker thread for play runs
+    setTimeout(() => {
+      nodepat.pauseSync();
+      nodepat.skipSync();
+    }, 100);
 
-        return playPromise;
-    }).timeout(TIMEOUT);
+    return playPromise;
+  }).timeout(TIMEOUT);
 });
