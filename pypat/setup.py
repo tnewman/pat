66cd9c442c76a9a73663beb65e6a62494b72f65e
Thomas Newman <tnewman@users.noreply.github.com>
2020-03-19 21:51:43 -0400

    Use pkg-config for Python build

--- pypat/setup.py
+++ pypat/setup.py
@@ -1,26 +1,62 @@
 from setuptools import setup, Extension
 
+import shutil
+import subprocess
+import sys
 import version
+
+include_dirs = []
+libraries = []
+
+
+def get_pkg_config_include_dir(package_name):
+    # Convert -I/path/to/include\n to /path/to/include
+    return subprocess.check_output(['pkg-config', package_name, '--cflags-only-I'])[2:-1].decode('utf-8')
+
+
+def get_pkg_config_library(package_name):
+    # Convert -llib\n to lib
+    return subprocess.check_output(['pkg-config', package_name, '--libs'])[2:-1].decode('utf-8')
+
+
+if sys.platform != 'win32':
+    if shutil.which('pkg-config') is None:
+        raise Exception('pkg-config is required on UNIX-like platforms!')
+
+    include_dirs = [
+        get_pkg_config_include_dir('libavcodec'),
+        get_pkg_config_include_dir('libavdevice'),
+        get_pkg_config_include_dir('libavformat'),
+        get_pkg_config_include_dir('libavutil'),
+        get_pkg_config_include_dir('libswresample'),
+        get_pkg_config_include_dir('sdl2'),
+    ]
+
+    libraries = [
+        get_pkg_config_library('libavcodec'),
+        get_pkg_config_library('libavdevice'),
+        get_pkg_config_library('libavformat'),
+        get_pkg_config_library('libavutil'),
+        get_pkg_config_library('libswresample'),
+        get_pkg_config_library('sdl2'),
+    ]
 
 _pypat = Extension('_pypat',
                    include_dirs=[
-                        'pypat/libpat/libpat/include',
-                        'pypat/libpat/libpat/src'
+                       'pypat/libpat/libpat/include',
+                       'pypat/libpat/libpat/src',
+                       *include_dirs
                    ],
                    libraries=[
-                        'avcodec',
-                        'avformat',
-                        'avutil',
-                        'swresample',
-                        'SDL2'
+                       *libraries,
                    ],
                    sources=[
-                        'pypat/_pypat.c',
-                        'pypat/libpat/libpat/src/pat.c',
-                        'pypat/libpat/libpat/src/pat_audio_device.c',
-                        'pypat/libpat/libpat/src/pat_decode.c',
-                        'pypat/libpat/libpat/src/pat_error.c',
-                        'pypat/libpat/libpat/src/pat_ring_buffer.c'
+                       'pypat/_pypat.c',
+                       'pypat/libpat/libpat/src/pat.c',
+                       'pypat/libpat/libpat/src/pat_audio_device.c',
+                       'pypat/libpat/libpat/src/pat_decode.c',
+                       'pypat/libpat/libpat/src/pat_error.c',
+                       'pypat/libpat/libpat/src/pat_ring_buffer.c'
                    ])
 
 with open('README.md', 'r') as fh:
